807c288bf371b23f4248a4fcf2891dc5e785245d
yunnyang <yun.j.1515017@gmail.com>
2020-01-12 12:34:29 +0900

    [YunJeong Kim][LeetCode][5] Longest Palindromic Substring

--- medium/leetCode5.py
+++ medium/leetCode5.py
@@ -1,44 +1,15 @@
 #5. Longest Palindromic Substring
-# This code trigger time limit exceeded and Memory limit exceeded
+
 
 class Solution:
-    cache = dict()
-
-    def longestPalindrome(self, s: str) -> str:
-        parlindrome = []
-
-        for i in range(0, len(s)):
-            longest = []
-            for j in range(i, len(s)):
-                longest.append(s[j])
-                if self.checkPalindrome(longest) == True:
-                    if len(longest) == len(s):
-                        return ''.join(longest)
-
-                    parlindrome.append(''.join(longest))
-
-        result = ""
-        for p in parlindrome:
-            if len(p) >= len(result):
-                result = p
-
-        return result
-
-    def checkPalindrome(self, s: str) -> str:
-        key = str(s)
-
-        if len(set(s)) == 1:
-            self.cache[key] = True
-            return True
-
-        if key in self.cache:
-            return self.cache[key]
-
-        else:
-            for i in range(0, int(len(s) / 2)):
-                if s[i] != s[~i]:
-                    self.cache[key] = False
-                    return False
-
-            self.cache[key] = True
-            return True
+    class Solution:
+        def longestPalindrome(self, s: str) -> str:
+            m = ''  # Memory to remember a palindrome
+            for i in range(len(s)):  # i = start, O = n
+                for j in range(len(s), i, -1):  # j = end, O = n^2
+                    if len(m) >= j - i:  # To reduce time
+                        break
+                    elif s[i:j] == s[i:j][::-1]:
+                        m = s[i:j]
+                        break
+            return m
