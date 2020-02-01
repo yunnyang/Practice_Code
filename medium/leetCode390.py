a0b80b069791a6a6193bbd3ddc14547c4f86b9d6
yunnyang <yun.j.1515017@gmail.com>
2020-02-01 22:21:14 +0900

    [YunJeong Kim][LeetCode][390] Elimination game - more efficient

--- medium/leetCode390.py
+++ medium/leetCode390.py
@@ -2,52 +2,19 @@
 
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        flag = 1  # odd
+        L2R = 1
+        head = 1
+        remaining = n
+        steps = 1
 
-        odd = []
-        even = []
+        while remaining > 1:
+            # if L2R == 1 or remaining & 1:
+            if L2R == 1 or remaining % 2 == 1:
+                head += steps
 
-        for num in range(1, n + 1):
-            if flag == 1:
-                odd.append(num)
-            elif flag == -1:
-                even.append(num)
+            remaining //= 2
+            steps *= 2
+            # L2R ^= 1
+            L2R = 1 - L2R
 
-            flag = flag * -1
-
-        left = []
-
-        if len(even) == 1:
-            return even.pop()
-
-        flag = 1  # init
-
-        while True:
-
-            if len(odd) == 1 or len(even) == 1:
-                if len(odd) == 1:
-                    return odd.pop()
-
-                if len(even) == 1:
-                    return even.pop()
-
-            if flag == 1:
-                nums = even
-                odd = [i for i in nums if nums.index(i) % 2 == 0]
-
-                if len(odd) > len(nums) / 2:
-                    left = list(set(nums) - set(odd))
-                    left.sort()
-                    odd = left
-
-
-            elif flag == -1:
-                nums = odd
-                even = [i for i in nums if nums.index(i) % 2 == 1]
-
-                if len(even) > len(nums) / 2:
-                    left = list(set(nums) - set(even))
-                    left.sort()
-                    even = left
-
-            flag = flag * -1
+        return head
